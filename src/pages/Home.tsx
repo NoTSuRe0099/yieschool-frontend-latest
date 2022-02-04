b8fa1bf064c54178e16128a120a04d13b600c193
Mosiur Rahman <mosiur.404.rahman@gmail.com>
2022-02-04 15:37:50 +0600

    added Carousel to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,33 @@
 import YIEButton from "../YIEUI/YIEButton";
+import YIECarousel from "../YIEUI/YIECarousel";
 import { YIETypography } from "../YIEUI/YIETypography";
+
+export const CarouselData = [
+  {
+    url: "/",
+    title: "test",
+    image:
+      "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    url: "/",
+    title: "test",
+    image:
+      "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    url: "/",
+    title: "test",
+    image:
+      "https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80",
+  },
+  {
+    url: "/",
+    title: "test",
+    image:
+      "https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+  },
+];
 
 const Home = () => {
   return (
@@ -7,6 +35,7 @@ const Home = () => {
       Home Page
       <YIEButton content="Button One" bgColor="red" />
       <YIEButton content="Button One" bgColor="blue" />
+      {/* Typography starts here */}
       <YIETypography component="h1" variant="h1">
         H1 - Lorem ipsum dolor sit amet.
       </YIETypography>
@@ -36,6 +65,8 @@ const Home = () => {
       >
         with color- Lorem ipsum dolor sit amet.
       </YIETypography>
+      {/* slider starts here */}
+      <YIECarousel slides={CarouselData} />
     </div>
   );
 };
